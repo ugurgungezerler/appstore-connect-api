d80fbcfc347daa9024daae82d41e406096985c43
ugurgungezerler <ugur@enrichmobile.app>
2020-09-30 14:01:30 +0300

    apps method

--- src/Api/App.php
+++ src/Api/App.php
@@ -8,6 +8,14 @@ class App extends AbstractApi
 {
     public function all(array $params = [], $id = null)
     {
+        if (!$id) {
+            return $this->get("/apps", $params);
+        }
+        return $this->get("/apps/{$id}", $params);
+    }
+
+    public function show($id, array $params = [])
+    {
         return $this->get("/apps/{$id}", $params);
     }
 }
